ee5834e9a29694765e0671ecdf82f6772907901a
Raphael Costa <raphael@raphaelcosta.net>
2024-07-28 15:37:33 -0300

    move to catppucin

--- init.lua
+++ init.lua
@@ -785,16 +785,16 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'catppuccin-mocha'
 
       -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
   },
 
@@ -871,7 +871,6 @@ require('lazy').setup({
     end,
   },
 
-  'arzg/vim-colors-xcode',
   'github/copilot.vim',
   {
     'mrjones2014/smart-splits.nvim',
@@ -958,8 +957,6 @@ vim.keymap.set('n', '<C-l>', function()
   require('smart-splits').move_cursor_right()
 end, { desc = 'Move to right split' })
 require('oil').setup()
-
-vim.cmd.colorscheme 'xcodedarkhc'
 
 vim.keymap.set('n', '<C-h>', function()
   require('smart-splits').move_cursor_left()
